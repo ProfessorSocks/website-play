0537f8eca43a60c568a9b260e6ee2c2e0612c286
ProfessorSocks <124716422+ProfessorSocks@users.noreply.github.com>
2023-08-09 14:30:07 -0700

    added better format

--- frontend/play.js
+++ frontend/play.js
@@ -30,3 +30,22 @@ function collectData(form) {
 
 
 }
+
+
+function resizeText() {
+    const textElement = document.getElementById("resize-text");
+    const container = document.querySelector(".text-container");
+
+    // Calculate the new font size based on the container's width
+    const containerWidth = container.offsetWidth;
+    const newFontSize = containerWidth * 0.05; // Adjust the scaling factor as needed
+
+    // Apply the new font size to the text element
+    textElement.style.fontSize = `${newFontSize}px`;
+}
+
+// Call the resizeText function when the window is resized
+window.addEventListener("resize", resizeText);
+
+// Initial call to resizeText when the page loads
+window.addEventListener("load", resizeText);
